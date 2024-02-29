0498d29e32a786e09ce9b5121554437384723bbd
Remi Debruyne <remidebruyne@hotmail.com>
2024-02-29 13:47:44 +0100

    Ajoutez des fichiers projet.

---
+++ Controllers/FakeContact.cs
@@ -0,0 +1,62 @@
+﻿using ContactApiDTO.Data;
+using ContactApiDTO.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakeContactController : ControllerBase
+    {
+        private readonly AppDbContext _fakeDb;
+
+        public FakeContactController(AppDbContext fakeDb)
+        {
+            _fakeDb = fakeDb;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var crepes = _fakeDb.Contacts;
+
+            if (crepes.Any()) // la liste est-elle non vide
+                return Ok(crepes); // statuscode 200
+
+            //return NotFound(); // 404 => implique que le fait de ne pas trouver de crêpes soit une erreur
+            return NoContent(); // 204 => implique que le fait de ne pas trouver de crêpes soit une réussite
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var crepe = _fakeDb.Contacts.FirstOrDefault(c => c.Id == id);
+
+            if (crepe == null)
+                return NotFound(new
+                {
+                    Message = "Crepe non trouvée !"
+                });
+
+            return Ok(new
+            {
+                Message = "Crepe trouvée !",
+                Crepe = crepe
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Contact contact)
+        //public IActionResult Post([FromForm]Crepe crepe) // si formulaire
+        {
+            _fakeDb.Contacts.Add(contact);
+
+            //return Ok("Crepe ajoutée");
+            return CreatedAtAction(nameof(GetById), new { id = contact.Id }, "Crepe ajoutée"); // meilleure version à utiliser de préférence
+            //return Created($"api/Crepe/{crepe.Id}", "Crepe ajoutée");
+
+            // dans le cas ou l'ajout aura échoué, il convient de retourner un BadRequest() => 400
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ContactApiDTO.Models;
+
+namespace ContactApiDTO.Data
+{
+    public class AppDbContext
+    {
+        public List<Contact> Contacts { get; set; } = new()
+        {
+                       new Contact { Id = 1, FirstName = "John", LastName = "Doe", BirthDate = new DateTime(1985, 7, 15), Gender = "M" },
+            new Contact { Id = 2, FirstName = "Jane", LastName = "Smith", BirthDate = new DateTime(1990, 5, 25), Gender = "F" },
+            new Contact { Id = 3, FirstName = "Alice", LastName = "Johnson", BirthDate = new DateTime(1978, 11, 3), Gender = "F" },
+            new Contact { Id = 4, FirstName = "Bob", LastName = "Brown", BirthDate = new DateTime(2000, 9, 10), Gender = "M" }
+        };
+        public AppDbContext()
+        {
+        }
+#nullable disable
+        
+#nullable enable
+    }
+}
